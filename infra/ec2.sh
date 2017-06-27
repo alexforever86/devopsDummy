7ba561a1f52a4a086301624eb53260ba19631f7d
AlexForever86 <alexforever86@gmail.com>
2017-06-27 20:34:32 +0530

    CloudFormation to create infrastructure

---
+++ infra/ec2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Login with aws credential
+echo "Creating pem file"
+# aws ec2 create-key-pair --key-name devops-dummy > devops-dummy.pem
+# chmod 600 devops-dummy.pem
+
+echo "Launching Stack"
+aws cloudformation create-stack --stack-name devops-dummy --template-body file://main.yaml \
+--parameters ParameterKey=NumberOfInstances,ParameterValue=$1 \
+ParameterKey=DummyInstanceType,ParameterValue=$2
+
+
+# ./ec2.sh 5 t2.micro
